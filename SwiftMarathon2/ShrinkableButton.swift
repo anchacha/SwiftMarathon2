32cf917c386d8db98276649a8c729899c5ec04ad
Anton Charny <anton.charny@perimeter81.com>
2023-03-05 11:02:12 +0200

    Shrinkable button class

--- SwiftMarathon2/ShrinkableButton.swift
+++ SwiftMarathon2/ShrinkableButton.swift
@@ -5,4 +5,18 @@
 //  Created by Anton Charny on 05/03/2023.
 //
 
-import Foundation
+import UIKit
+
+class ShrinkableButton: UIButton {
+    
+    override var isHighlighted: Bool {
+        didSet {
+            let transform = isHighlighted ? 0.9 : 1
+            UIView.animate(withDuration: 0.3, delay: 0,
+                           usingSpringWithDamping: 1, initialSpringVelocity: 1,
+                           options: [.allowUserInteraction, .beginFromCurrentState]) {
+                self.transform = CGAffineTransform(scaleX: transform, y: transform)
+            }
+        }
+    }
+}
